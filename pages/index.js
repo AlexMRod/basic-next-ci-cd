84ea68f7326cc0cc17ac18888fba0a3c4a9fed8b
AlexMRod <65395899+AlexMRod@users.noreply.github.com>
2024-12-12 20:27:13 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -22,6 +22,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+    <h1> CHICHI PELADO </h1>
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
